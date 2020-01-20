77c83a902d2a3e9ffe4412de9fb0b7d4bcfebdb9
aymenjelal <aymen2jelal@gmial.com>
2020-01-20 11:37:42 +0300

    first commit

---
+++ MyCollege/Migrations/201902011246306_IndtructorMigration.cs
@@ -0,0 +1,16 @@
+namespace MyCollege.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IndtructorMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
